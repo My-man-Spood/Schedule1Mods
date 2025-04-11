05d3b8ccb08b1f063b74bd29183b0cc02a060ffa
jeremie <ownedstuff@hotmail.fr>
2025-04-10 20:46:19 -0400

    Made LargerTrashRadius

---
+++ Spood.Mono.LargerTrashRadius/Spood.Mono.LargerTrashRadius/Mod.cs
@@ -0,0 +1,28 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(Spood.Mono.LargerTrashRadius.Mod), "Spood.Mono.LargerTrashRadius", "1.0.0", "Spood", null)]
+[assembly: MelonGame("TVGS", "Schedule I")]
+namespace Spood.Mono.LargerTrashRadius;
+
+public class Mod : MelonMod
+{
+    public const string PreferencesCategory = "Spood_LargerTrashRadius";
+    public const string PreferencesRadiusEntryName = "radius";
+    
+    public override void OnInitializeMelon()
+    {
+        LoggerInstance.Msg("Spood.Mono.LargerTrashRadius Initialized");
+        SetupPreferences();
+        HarmonyInstance.PatchAll();
+    }
+
+    private void SetupPreferences()
+    {
+        var cat = MelonPreferences.CreateCategory(PreferencesCategory);
+        if(cat.GetEntry(PreferencesRadiusEntryName) == null)
+        {
+            cat.CreateEntry<float>(PreferencesRadiusEntryName, 5.5f);
+            cat.SaveToFile();
+        }
+    }
+}
---
+++ Spood.Mono.LargerTrashRadius/Spood.Mono.LargerTrashRadius/PickupRadiusPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using ScheduleOne.ObjectScripts;
+
+namespace Spood.Mono.LargerTrashRadius
+{
+    [HarmonyPatch(typeof(TrashContainerItem), "Awake")]
+    class PickupRadiusPatch
+    {
+        private static void Prefix(TrashContainerItem __instance)
+        {
+            // get preferences from file
+            var cat = MelonPreferences.GetCategory(Mod.PreferencesCategory);
+            var radius = cat.GetEntry<float>(Mod.PreferencesRadiusEntryName).Value;
+
+            // patch trash can radius
+            __instance.PickupRadius = radius;
+        }
+    }
+}
