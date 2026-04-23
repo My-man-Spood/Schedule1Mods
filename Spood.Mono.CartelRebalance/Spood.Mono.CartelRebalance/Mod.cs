49aa11201710333f305f1cab0cc051b86a5ebd41
jeremie <ownedstuff@hotmail.fr>
2026-04-23 01:38:56 -0400

    Add CartelRebalance mod and update project dependencies
    - Introduces the CartelRebalance mod project to the Mono solution.
    - Adds a missing reference to ScheduleOne.Core in the PickyCustomers project.
    - Updates .gitignore to exclude bin and obj build directories.

---
+++ Spood.Mono.CartelRebalance/Spood.Mono.CartelRebalance/Mod.cs
@@ -0,0 +1,14 @@
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(Spood.Mono.CartelRebalance.Mod), "Spood.Mono.CartelRebalance", "1.0.0", "Spood", null)]
+[assembly: MelonGame("TVGS", "Schedule I")]
+namespace Spood.Mono.CartelRebalance;
+
+public class Mod : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        LoggerInstance.Msg("Spood.Mono.CartelRebalance Initialized");
+        HarmonyInstance.PatchAll();
+    }
+}
